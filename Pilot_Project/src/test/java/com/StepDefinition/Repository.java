27e67a7c6468fdcab53feee5e67c990cc9161a23
Murugan2002416 <117498964+Murugan2002416@users.noreply.github.com>
2023-07-24 12:53:00 +0530

    Add files via upload

---
+++ Pilot_Project/src/test/java/com/StepDefinition/Repository.java
@@ -0,0 +1,98 @@
+package com.StepDefinition;
+
+ 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+ 
+
+ 
+
+public class Repository   {
+    public static WebDriver driver;
+    //6.6 List new Advertisements
+//    public Repository(WebDriver driver)
+//    {
+//         this.driver = driver;
+//    PageFactory.initElements(driver, this);
+//    }
+        @FindBy(xpath = "//div[text()='Advertisements']")
+        public static WebElement advertisement;
+
+        @FindBy(xpath = "//*[text()='Show advertisements']")
+        public static WebElement showAdvertisement;
+
+        @FindBy(xpath = "//*[text()='Order by']")
+        public static WebElement orderBy;
+
+        @FindBy(id = "id_40_date")
+        public static WebElement lastPublished;
+
+
+
+
+
+        //6.7 Category Browsing
+
+           //click the advertisement WebElement
+
+        @FindBy(css = "*[id='id_6']")
+        public static WebElement keyWords; //enter the values 
+
+
+
+        //6.8  Multiple Advertisement Categories
+
+          //click the advertisement WebElement
+
+        //This will locate the first community  
+        @FindBy(xpath = "//*[@class=\"category-title-text\"]")
+        public static WebElement community;
+
+
+
+        //6.9 Custom advertisement fields
+
+           //click the advertisement WebElement
+        //
+        @FindBy(xpath = "//*[@class='sub-category-text']")
+        public static WebElement category;
+
+        @FindBy(xpath = "//*[@class='avatar-container full-size']")
+        public static WebElement item;
+
+
+
+        //7.1 verify gift 
+        @FindBy(xpath = "//*[@class='nav-item-text' and text()='My vouchers']")
+        public static WebElement voucher;
+
+        @FindBy(xpath = "//*[@class='table table-hover cursor-pointer']//tr[1]")
+        public static WebElement tableCol1;
+
+        @FindBy(xpath="//*[text()='Print']")
+        public static WebElement print;
+
+
+        //7.2 Buy and print voucher
+          //click the voucher
+
+        @FindBy(xpath = "//*[text()='Buy and email']")
+        public static WebElement buyEmail;
+
+        @FindBy(xpath = "//*[@class='table table-no-header table-hover cursor-pointer']//tr[1]")
+        public static WebElement email;
+
+        @FindBy(id = "id_99")
+        public static WebElement emailText;
+
+        @FindBy(id = "id_98")
+        public static WebElement amount;
+
+        @FindBy(css = "button[class^='btn d-flex']")
+        public static WebElement next;
+
+ 
+
+}
